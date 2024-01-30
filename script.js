2aff7f62e8b5ab373159cdbc5344520e6550c7bf
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-01-30 11:27:47 +0530

    script.js

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     const femaleRadio = document.querySelector('input[name="gender"][value="female"]');
     const gender = (maleRadio && maleRadio.checked) ? 'male' : (femaleRadio && femaleRadio.checked) ? 'female' : '';
 
+    const selectedFoods = document.querySelectorAll('input[name="food"]:checked');
   
+  
+  if (selectedFoods.length < 2) {
+    alert('Please choose at least 2 food options.');
+    return;
+  }
 
-
+  const foodValues = Array.from(selectedFoods).map(checkbox => checkbox.value);
+   const state = document.getElementById('state').value;
+    const country = document.getElementById('country').value;
     const tableBody = document.getElementById('dataTableBody');
     const newRow = tableBody.insertRow();
-    newRow.innerHTML = `<td>${firstName}</td><td>${lastName}</td><td>${address}</td><td>${pincode}</td><td>${gender}</td>`;
+    newRow.innerHTML = `<td>${firstName}</td><td>${lastName}</td><td>${address}</td><td>${pincode}</td><td>${gender}</td><td>${foodValues.join(',')}</td><td>${state}</td><td>${country}</td>`;
 
 
     document.getElementById('myForm').reset();
